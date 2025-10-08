c0805e708ee61672c497654e2902f2fcceac3905
roblabla <unfiltered@roblab.la>
2025-10-08 15:09:17 +0200

    Don't panic on empty cert list or unsupported format.

--- authenticode/src/signature.rs
+++ authenticode/src/signature.rs
@@ -258,14 +258,14 @@ impl AuthenticodeSignature {
         self.signed_data
             .certificates
             .as_ref()
-            .unwrap()
-            .0
-            .iter()
-            .map(|cert| {
+            .into_iter()
+            .map(|v| v.0.iter())
+            .flatten()
+            .flat_map(|cert| {
                 if let cms::cert::CertificateChoices::Certificate(cert) = cert {
-                    cert
+                    Some(cert)
                 } else {
-                    panic!()
+                    None
                 }
             })
     }
